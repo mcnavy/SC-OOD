f79043fce87683d9a1612a6575b65721c38f8df7
Afanasiy <rnibbler99@gmail.com>
2018-03-02 17:49:09 +0300

    Add files via upload

---
+++ MovieTheatre/src/model/TicketKiosk.java
@@ -0,0 +1,42 @@
+package model;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TicketKiosk {
+
+    private String name;
+    private List<Movie> movies;
+
+    // EFFECTS: TicketKiosk with name (name) is created
+    public TicketKiosk(String name) {
+        this.name = name;
+        movies = new LinkedList<>();
+    }
+
+    // getters
+    public String getName() {
+       return name;
+    }
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    //EFFECTS:  adds the movie to movies, unless it is already in movies.
+    //          if add is successful return true, otherwise return false.
+    public boolean addMovie(Movie m) {
+        if(!movies.contains(m))
+        {
+            movies.add(m);
+            return true;
+        }
+        return false;
+    }
+
+    //EFFECTS: calls MovieGoer's buyTicket method, passing m as a parameter.
+    public boolean sellTicket(MovieGoer mg, Movie m) {
+
+        return true;
+    }
+
+
+}
